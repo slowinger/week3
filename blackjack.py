36d6052f436b374cb447731fc473d15cf5ec94fd
slowinger <slowinger@uchicago.edu>
2015-01-24 10:51:43 -0600

    Added my solution to blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,120 +1,126 @@
-# Game of Blackjack
-
-# Simplified Rules (10 pts possible):
-# - Human player gets the first two cards
-# - Human player plays the rest of their hand
-# - Then computer gets next two cards
-# - Computer must take cards score  >= 17
-# - Computer must stand when score >= 17
-# - Aces always count as 11
-# - Human player loses if their score is > 21
-# - Computer loses if computer score is > 21
-# - Human player wins immediately if their score is exactly 21
-# - Computer wins immediately if their score is exactly 21
-# - If computer score is betwen 17 and 20, winner is determined by score
-# - If it's a tie, nobody wins.
-
-# Grading:
-# - 5 points for allowing a human user to play their complete hand
-# - 5 points for allowing the computer to play its hand
-
-# (Optional) Extras
-# [You don't get extra credit for these, but they're fun.]
-# - 1. Aces should count as 1 if counting as 11 would have made the score > 21
-# - 2. Initally, human and dealer both get two cards; one dealer card is face up
-# - 3. Allow the user to play as many games as they want
-# - 4. Dealing cards to the cmputer should have a dramatic, 4-second delay
-
-# Here's the psuedocode we wrote on the board in class:
-
-## Get a deck of cards
-
-## Shuffle the deck
-
-## Deal the first two cards to user
-
-## User can choose to take cards as long as score < 21
-
-## If user goes over 21, game is over.
-
-## If user reaches 21, game is over.
-
-## If user stands with less than 21, then it's the dealer's turn:
-
-##    Computer takes two cards
-##    Computer must take more cards while computer score < 17
-##    If computer score reached 21, computer wins.
-##    If computer score goes over 21, computer loses.
-##    If computer score is 17 to 20, winner is determined by higher score.
-
-
-
-
-
+﻿
 import random
-import time
+from time import sleep
 
-SUITS = "\u2663 \u2665 \u2666 \u2660".split()
-FACES = "A 2 3 4 5 6 7 8 9 10 J Q K".split()
 
-deck = []
-for suit in SUITS:
-  for face in FACES:
-    deck.append(face+suit)
+def draw(deck):
+    new_card = deck[0]
+    deck = deck[1:]
+    return new_card, deck
 
-random.shuffle(deck)
 
-def calculate_score(cards):
-  value = 0
-  for card in cards:
-    face = card[:-1]
-    if face in ['J', 'Q', 'K']:
-      points = 10
-    elif face == 'A':
-      points = 11
+def draw_evaluate(card, hand):
+    if card[0] in ['J', 'Q', 'K', '1']:
+        hand.append(10)
+    elif card[0] == 'A':
+        hand.append(11)
     else:
-      points = int(face)
-    value += points
-  return value
-
-# Deal two cards
-hand = [deck.pop(0), deck.pop(0)]
-score = calculate_score(hand)
-
-print("Your hand:", " ".join(hand))
-
-while score < 21 and input("Do you want another card? (y/n) ") == 'y':
-  hand.append(deck.pop(0))
-  score = calculate_score(hand)
-  print("Your hand:", ", ".join(hand))
-
-if score > 21:
-  print("You're busted!")
-elif score == 21:
-  print("You win!")
-else:
-  print("You have %s points." % score)
-  print()
-  dealer_hand = [deck.pop(0), deck.pop(0)]
-  dealer_score = calculate_score(dealer_hand)
-  print("Dealer has", " ".join(dealer_hand))
-  while dealer_score < 17:
-    print("The dealer will take another card...")
-    time.sleep(5)
-    dealer_hand.append(deck.pop(0))
-    dealer_score = calculate_score(dealer_hand)
-    print("Dealer now has", " ".join(dealer_hand))
-    time.sleep(3)
-
-  if dealer_score > 21:
-    print("The dealer busted! You win!")
-  elif dealer_score == 21:
-    print("The dealer got 21! You lose.")
-  elif dealer_score > score:
-    print("You lost.")
-  elif dealer_score == score:
-    print("It's a tie.... nobody wins this time.")
-  else:
-    print("You win!")
+        hand.append(int(card[0]))
 
 
+def sum_hand(hand):
+    add_hand = 0
+    for x in hand:
+        add_hand += x
+    return add_hand
+
+player_score = 0
+computer_score = 0
+exit_game = False
+while exit_game is not True:
+
+    player_hand = []
+    computer_hand = []
+    player_sum = 0
+    computer_sum = 0
+
+    # I used 7_cards.py to create the deck
+    suits = "\u2663 \u2665 \u2666 \u2660".split()
+    values = "A 2 3 4 5 6 7 8 9 10 J Q K".split()
+
+    deck = []
+    for suit in suits:
+        for face in values:
+            deck.append(face+suit)
+
+    random.shuffle(deck)
+    player_card_1 = deck[0]
+    computer_card_1 = deck[1]
+    player_card_2 = deck[2]
+    computer_card_2 = deck[3]
+    deck = deck[4:]
+    print("Player hand: %s %s" % (player_card_1, player_card_2))
+    print("The computer card 1:", computer_card_1)
+
+    draw_evaluate(player_card_1, player_hand)
+    draw_evaluate(player_card_2, player_hand)
+    draw_evaluate(computer_card_1, computer_hand)
+    draw_evaluate(computer_card_2, computer_hand)
+    player_sum = sum_hand(player_hand)
+    computer_sum = sum_hand(computer_hand)
+
+    if player_sum != 21 and computer_sum != 21:
+        exit = False
+        while player_sum < 21 and exit is False:
+            player_draw = input('Do you want another card? (y/n)\n')
+            if player_draw == 'y':
+                new_card, deck = draw(deck)
+                draw_evaluate(new_card, player_hand)
+                if sum_hand(player_hand) > 21:
+                    for index, value in enumerate(player_hand):
+                        if value == 11:
+                            player_hand[index] = 1
+                player_sum = sum_hand(player_hand)
+                print(
+                    "Drawn Card: %s, player_total: %s"
+                    % (new_card, player_sum))
+            elif player_draw == 'n':
+                exit = True
+            else:
+                print("ERROR: invalid input")
+
+        if player_sum <= 21:
+            print(
+                "computer hand: %s %s, computer total: %s"
+                % (computer_card_1, computer_card_2, computer_sum))
+            while computer_sum < 17:
+                new_card, deck = draw(deck)
+                draw_evaluate(new_card, computer_hand)
+                if sum_hand(computer_hand) > 21:
+                    for index, value in enumerate(computer_hand):
+                        if value == 11:
+                            computer_hand[index] = 1
+                computer_sum = sum_hand(computer_hand)
+                sleep(4)
+                print(
+                    "The computer drew: %s, the computer total: %s"
+                    % (new_card, computer_sum))
+            if computer_sum > 21:
+                player_score += 1
+                print("Congratulations you win, The computer busted!")
+            elif computer_sum < player_sum:
+                player_score += 1
+                print("Congratulations you win!")
+            elif computer_sum > player_sum:
+                computer_score += 1
+                print("Sorry the computer won!")
+            elif computer_sum == player_sum:
+                print("You tied!")
+        else:
+            computer_score += 1
+            print("Sorry you busted, the computer wins!")
+    elif player_sum == 21 and computer_sum != 21:
+        player_score += 1
+        print("Congratulations you got Blackjack!")
+    elif player_sum != 21 and computer_sum == 21:
+        computer_score += 1
+        print("Sorry The computer got Blackjack!")
+    else:
+        print("You tied!")
+    sleep(3)
+    print(
+        "score:\nplayer: %s\ncomputer: %s\n"
+        % (player_score, computer_score))
+    play_again = input('Do you want to play again? (y/n)\n')
+    if play_again == 'n':
+        exit_game = True
